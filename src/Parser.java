bf6be49935f71201e027791087ea0b444a367910
Brandon Forster <brandonforster@gmail.com>
2012-12-01 15:04:30 -0500

    constructor eats the header line of input. added debugging main

--- src/Parser.java
+++ src/Parser.java
@@ -5,6 +5,35 @@
 //3 December 2012
 //Parsing the XML file
 
+import java.io.*;
+import java.util.*;
+
 public class Parser {
 
+	public Parser(String filename) throws FileNotFoundException
+	{
+		File xlsFile = new File(filename);
+		
+		//this method call throws the FileNotFound
+		Scanner xlIn = new Scanner(xlsFile);
+		
+		//eat the first line of input until we get to the main input
+		xlIn.nextLine();
+		
+		//start reading in the whole input
+		while (xlIn.hasNext() == true)
+		{
+			
+		}
+	}
+	
+	//TODO remove this main, it is for debugging
+	public static void main(String[] args)
+	{
+		try {
+			new Parser("WEOOct2012all.xls");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
+	}
 }
