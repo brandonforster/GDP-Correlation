4af0cd8a6c1f548b44c56b208f3e1c149a7e6dd2
Brandon Forster <brandonforster@gmail.com>
2012-12-01 14:36:16 -0500

    Added xls file. Added empty parser class.

---
+++ src/Parser.java
@@ -0,0 +1,10 @@
+//Brandon Forster, Robert Millward,
+//Matt Head
+//COP 3503 Extra Credit Assignment
+//Huffman Encoding and Decoding
+//3 December 2012
+//Parsing the XML file
+
+public class Parser {
+
+}
